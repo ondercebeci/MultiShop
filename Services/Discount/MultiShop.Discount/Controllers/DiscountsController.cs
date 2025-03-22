2b1becb44fdbfdc0ede57b0365c65cde8bf16caa
ondercebeci <69725787+ondercebeci@users.noreply.github.com>
2025-03-22 14:43:37 +0300

    Discount Mikroservis crud işlemleri

---
+++ Services/Discount/MultiShop.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.Dtos;
+using MultiShop.Discount.Services;
+
+namespace MultiShop.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountsController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> DiscountCouponList()
+        {
+            var values = await _discountService.GetAllDiscountCouponAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDiscountCouponById(int id)
+        {
+            var values = await _discountService.GetByIdDiscountCouponAsync(id);
+            return Ok(values);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDiscountCoupon(int id)
+        {
+            await _discountService.DeleteDiscountCouponAsync(id);
+            return Ok("Kupon Başarılı Bir Şekilde Silindi");
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscountCoupon(CreateDiscountCouponDto createDiscountCouponDto)
+        {
+            await _discountService.CreateDiscountCouponAsync(createDiscountCouponDto);
+            return Ok("Kupon Başarılı Bir Şekilde Oluşturuldu");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateDiscountCoupon(UpdateDiscountCouponDto updateDiscountCouponDto)
+        {
+            await _discountService.UpdateDiscountCouponAsync(updateDiscountCouponDto);
+            return Ok("Kupon Başarılı Bir Şekilde Güncellendi");
+        }
+    }
+}
