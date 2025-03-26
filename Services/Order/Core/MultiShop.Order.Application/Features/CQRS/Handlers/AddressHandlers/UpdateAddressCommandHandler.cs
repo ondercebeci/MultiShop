35520f19c54819977c6b543bab1311b351859d78
ondercebeci <69725787+ondercebeci@users.noreply.github.com>
2025-03-26 17:06:14 +0300

    address ordering orderdetail repo ve api crud işlemleri.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class UpdateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public UpdateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateAddressCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.AddressID);
+            values.Detail = command.Detail;
+            values.Disctrict = command.Disctrict;
+            values.City = command.City;
+            values.AddressID = command.AddressID;
+            values.UserID = command.UserID;
+            await _repository.UpdateAsync(values);
+            
+        }
+    }
+}
